bbad819fddd4811f5d8287ab57551b20485855ea
Zapadynsky <e.zapadinsky@gmail.com>
2023-10-02 11:15:31 +0200

    Add requirements.txt

--- goods/views.py
+++ goods/views.py
@@ -1,29 +1,35 @@
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+
 from .models import Product, Category
 from .serializers import ProductSerializer, CategorySerializer
 
 
-class CategoryListView(generics.ListCreateAPIView):
+class BaseAPIView(generics.GenericAPIView):
+    permission_classes = [IsAuthenticated]
+
+
+class CategoryListView(BaseAPIView, generics.ListCreateAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
 
-class CategoryDetailView(generics.RetrieveUpdateDestroyAPIView):
+class CategoryDetailView(BaseAPIView, generics.RetrieveUpdateDestroyAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
 
 
-class ProductListView(generics.ListCreateAPIView):
+class ProductListView(BaseAPIView, generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
 
-class ProductDetailView(generics.RetrieveUpdateDestroyAPIView):
+class ProductDetailView(BaseAPIView, generics.RetrieveUpdateDestroyAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
 
-class ProductByCategoryView(generics.ListAPIView):
+class ProductByCategoryView(BaseAPIView, generics.ListAPIView):
     serializer_class = ProductSerializer
 
     def get_queryset(self):
